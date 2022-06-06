cbb0a713122eb10171273f2fea60019723be42aa
Aaron Enyetu <aaronenyetu@gmail.com>
2022-06-06 16:32:38 -0700

    Made some changes

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import backgroundImg from '../background.png';
 import './Main.css';
 import ZodiacCard from '../ZodiacCard/ZodiacCard';
-import { zodiac } from '../data';
+
 
 export default function Main({ zodiacs }) {
   return (
--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -4,7 +4,7 @@ import './ZodiacCard.css';
 export default function ZodiacCard({ name, dates }) { 
   return (
   /* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */
-    <div className='zodiac-card'>
+    <div className="zodiac-card">
       <h1>{name}</h1>
       <img src={`/images/${name}.png`} />
       <p>{dates}</p>
